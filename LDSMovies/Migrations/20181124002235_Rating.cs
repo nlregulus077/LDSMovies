e5ecb91703c3a4d3a5548e5629c0dfed11326c57
Jeanina Lao <lao13001@byui.edu>
2018-11-23 17:54:01 -0700

    Mostly done but still need to add extra stuff

---
+++ LDSMovies/Migrations/20181124002235_Rating.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LDSMovies.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
---
+++ LDSMovies/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LDSMovies.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize (IServiceProvider serviceProvider)
+        {
+            using (var context = new LDSMoviesContext(serviceProvider.GetRequiredService<DbContextOptions<LDSMoviesContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("2011-6-03"),
+                        Genre = "Adventure",
+                        Rating = "PG" 
+                    },
+
+                    new Movie
+                    {
+                        Title = "The R.M.",
+                        ReleaseDate = DateTime.Parse("2003-1-31"),
+                        Genre = "Comedy",
+                        Rating = "G"  
+                    }
+                    
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
